db34e48dd8d934d36f28d35e3d54a14f67c4d2df
RedLeader962 <redleader962@gmail.com>
2025-06-30 12:14:49 -0400

    ci: add locale configuration and update Git config in test script
    - Added `locales` package installation and locale configuration to set
      `en_US.UTF-8` for consistent language settings across environments.
    - Updated Git global configuration to mark all directories as safe,
      resolving potential issues with submodules.
    
    Issue TNP-48

--- tests/run_all_dryrun_and_tests_scripts.bash
+++ tests/run_all_dryrun_and_tests_scripts.bash
@@ -11,6 +11,7 @@ if [[ ${TEAMCITY_VERSION} ]]; then
   # Assuming is run in a TC docker run wrapper
   apt-get update \
     && apt-get install --assume-yes --no-install-recommends \
+        locales \
         sudo \
         lsb-release \
         curl \
@@ -22,6 +23,12 @@ if [[ ${TEAMCITY_VERSION} ]]; then
         fontconfig \
         software-properties-common \
     && rm -rf /var/lib/apt/lists/*
+
+  locale-gen en_US en_US.UTF-8 && update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+
+  # This is required in our case since we deal with git submodule
+  git config --global --add safe.directory "*"
+
 fi
 
 tnp_root=$(git rev-parse --show-toplevel)
