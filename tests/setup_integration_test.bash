2f0ac8a0ccd7176eac48f2f0b299a211841c7d27
RedLeader962 <redleader962@gmail.com>
2025-06-30 11:27:46 -0400

    ci: update GitHub CLI installation logic in setup script
    - Replaced Linux-specific check with TeamCity environment variable check.
    - Updated installation steps to enhance robustness and reliability.
    - Ensures compatibility with TeamCity and reduces potential errors.
    
    Issue TNP-48, TNP-49

--- tests/setup_integration_test.bash
+++ tests/setup_integration_test.bash
@@ -17,13 +17,18 @@ pushd "$(pwd)" >/dev/null || exit 1
 function tnp::install_gh_cli_on_ci() {
   # Official doc: https://github.com/cli/cli/blob/trunk/docs/install_linux.md
 
-  if [[ $(uname) == "Linux" ]] && ! command -v gh &> /dev/null; then
+  if [[ ${TEAMCITY_VERSION} ]] && ! command -v gh &> /dev/null; then
     echo "Test is run on a TeamCity server, install GitHub cli"
     {
-      curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-      sudo apt update
-      sudo apt install gh -y
+      (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+        && sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+        && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+        && sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+        && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+        && sudo apt update \
+        && sudo apt install gh -y ;
     } || { n2st::print_msg_error "Failed to install GitHub cli!" ; return 1 ; }
   fi
 
